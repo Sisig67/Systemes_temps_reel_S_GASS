e2e66bc1048b2a93550cf2ce791f34f9498740b7
Sisig67 <simeon.gass@gmail.com>
2025-09-09 09:34:57 +0200

    test git

--- src/main/java/fr/insa/gass/str/Main.java
+++ src/main/java/fr/insa/gass/str/Main.java
@@ -3,5 +3,6 @@ package fr.insa.gass.str;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        // TODO code application logic here
     }
 }
